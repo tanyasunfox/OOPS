323d4ae796323361008f0ba05bb00671006559aa
tanyasunfox <126673902+tanyasunfox@users.noreply.github.com>
2023-04-06 18:05:10 +0530

    Add files via upload
    gargle file added

---
+++ BUILD/src/main/java/org/example/Child.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Child {
+    public static void main(String[] args) {
+        System.out.println("child class1");
+    }
+}
---
+++ BUILD/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Tute obj= new Tute();
+        System.out.println(obj.name("tanya"));
+        System.out.println("Hello World");
+
+
+
+    }
+}
